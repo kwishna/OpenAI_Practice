db6d50b77935304dc29b54fcaee9153224ed316e
kwishna <kwishna@gmail.com>
2024-03-30 15:45:33 +0530

    openai function calls uses

--- openai_examples/Q&A.py
+++ openai_examples/Q&A.py
@@ -31,15 +31,15 @@ import openai
 openai.api_key = "sk-"
 
 response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=
-  "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:",
-  temperature=0,
-  max_tokens=100,
-  top_p=1,
-  frequency_penalty=0.0,
-  presence_penalty=0.0,
-  stop=["\n"])
+    model="text-davinci-003",
+    prompt=
+    "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:",
+    temperature=0,
+    max_tokens=100,
+    top_p=1,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    stop=["\n"])
 
 # ----------------------------------------------------------------------------------
 # Grammar Correct Prompt:-
@@ -49,13 +49,13 @@ response = openai.Completion.create(
 # She no went to the market.
 
 response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="Correct this to standard English:\n\nShe no went to the market.",
-  temperature=0,
-  max_tokens=60,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0)
+    model="text-davinci-003",
+    prompt="Correct this to standard English:\n\nShe no went to the market.",
+    temperature=0,
+    max_tokens=60,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0)
 
 # -----------------------------------------------------------------------------------
 # Translates difficult text into simpler concepts. Prompt :-
@@ -65,14 +65,14 @@ response = openai.Completion.create(
 # Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus.
 
 response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=
-  "Summarize this for a second-grade student:\n\nJupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus.",
-  temperature=1,
-  max_tokens=64,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0)
+    model="text-davinci-003",
+    prompt=
+    "Summarize this for a second-grade student:\n\nJupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus.",
+    temperature=1,
+    max_tokens=64,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0)
 
 print(response.choices[0].message['content'])
 
@@ -95,15 +95,15 @@ print(response.choices[0].message['content'])
 # """
 
 response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=
-  "\"\"\"\nUtil exposes the following:\nutil.openai() -> authenticates & returns the openai module, which has the following functions:\nopenai.Completion.create(\n    prompt=\"<my prompt>\", # The prompt to start completing from\n    max_tokens=123, # The max number of tokens to generate\n    temperature=1.0 # A measure of randomness\n    echo=True, # Whether to return the prompt in addition to the generated completion\n)\n\"\"\"\nimport util\n\"\"\"\nCreate an OpenAI completion starting from the prompt \"Once upon an AI\", no more than 5 tokens. Does not include the prompt.\n\"\"\"\n",
-  temperature=0,
-  max_tokens=64,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0,
-  stop=["\"\"\""])
+    model="text-davinci-003",
+    prompt=
+    "\"\"\"\nUtil exposes the following:\nutil.openai() -> authenticates & returns the openai module, which has the following functions:\nopenai.Completion.create(\n    prompt=\"<my prompt>\", # The prompt to start completing from\n    max_tokens=123, # The max number of tokens to generate\n    temperature=1.0 # A measure of randomness\n    echo=True, # Whether to return the prompt in addition to the generated completion\n)\n\"\"\"\nimport util\n\"\"\"\nCreate an OpenAI completion starting from the prompt \"Once upon an AI\", no more than 5 tokens. Does not include the prompt.\n\"\"\"\n",
+    temperature=0,
+    max_tokens=64,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    stop=["\"\"\""])
 
 print(response.choices[0].message['content'])
 
@@ -118,19 +118,19 @@ print(response.choices[0].message['content'])
 # Reach out to the ski store and figure out if I can get my skis fixed before I leave on Thursday
 
 response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=
-  "Convert this text to a programmatic command:\n\nExample: Ask Constance if we need some bread\nOutput: send-msg `find constance` Do we need some bread?\n\nReach out to the ski store and figure out if I can get my skis fixed before I leave on Thursday",
-  temperature=0,
-  max_tokens=100,
-  top_p=1.0,
-  frequency_penalty=0.2,
-  presence_penalty=0.0,
-  stop=["\n"])
+    model="text-davinci-003",
+    prompt=
+    "Convert this text to a programmatic command:\n\nExample: Ask Constance if we need some bread\nOutput: send-msg `find constance` Do we need some bread?\n\nReach out to the ski store and figure out if I can get my skis fixed before I leave on Thursday",
+    temperature=0,
+    max_tokens=100,
+    top_p=1.0,
+    frequency_penalty=0.2,
+    presence_penalty=0.0,
+    stop=["\n"])
 
 print(response.choices[0].message['content'])
 
-#-------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
 # Translates English text into French, Spanish and Japanese. Prompt:-
 
 # Translate this into 1. French, 2. Spanish and 3. Japanese:
@@ -141,14 +141,14 @@ print(response.choices[0].message['content'])
 # Sample re
 
 response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=
-  "Translate this into 1. French, 2. Spanish and 3. Japanese:\n\nWhat rooms do you have available?\n\n1.",
-  temperature=0.3,
-  max_tokens=100,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0)
+    model="text-davinci-003",
+    prompt=
+    "Translate this into 1. French, 2. Spanish and 3. Japanese:\n\nWhat rooms do you have available?\n\n1.",
+    temperature=0.3,
+    max_tokens=100,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0)
 
 print(response.choices[0].message['content'])
 
@@ -164,14 +164,16 @@ print(response.choices[0].message['content'])
 ### A query to list the names of the departments which employed more than 10 employees in the last 3 months SELECT
 
 response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=
-  "### Postgres SQL tables, with their properties:\n#\n# Employee(id, name, department_id)\n# Department(id, name, address)\n# Salary_Payments(id, employee_id, amount, date)\n#\n### A query to list the names of the departments which employed more than 10 employees in the last 3 months\nSELECT",
-  temperature=0,
-  max_tokens=150,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0,
-  stop=["#", ";"])
+    model="text-davinci-003",
+    prompt=
+    "### Postgres SQL tables, with their properties:\n#\n# Employee(id, name, department_id)\n# Department(id, name, address)\n# Salary_Payments(id, employee_id, amount, date)\n#\n### A query to list the names of the departments which employed more than 10 employees in the last 3 months\nSELECT",
+    temperature=0,
+    max_tokens=150,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    stop=["#", ";"])
 
 print(response.choices[0].message['content'])
+
+## ----- TODO: Go through all the examples in openai websites
